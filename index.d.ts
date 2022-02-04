04836cb2f5d21f11b76f475b325c5ae5cb920e80
Nakarin Hansawattana <nakarinh14@gmail.com>
2022-02-04 11:45:27 +0700

    Moved index.ts to index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for omise-js 2.5.6
+// Project: https://github.com/saltyAom/omise-type
+// Definitions by: SaltyAom <https://github.com/saltyAom>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/**
+ * Third-party Omise.js TypeScript's typings support.
+ */
+
+import Omise from './src/lib/omise'
+import OmiseCard from './src/lib/omiseCard'
+
+export { Omise, OmiseCard }
